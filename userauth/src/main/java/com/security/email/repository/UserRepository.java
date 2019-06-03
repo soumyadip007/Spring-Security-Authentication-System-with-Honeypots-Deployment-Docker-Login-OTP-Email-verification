76aa2966272b239d30b983b3fae7d1c09ece06b1
soumyadip007 <soumyadip.note@gmail.com>
2019-06-03 14:46:50 +0530

    JpaRepo changed to CurdRepo

--- userauth/src/main/java/com/security/email/repository/UserRepository.java
+++ userauth/src/main/java/com/security/email/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.security.email.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.security.email.entity.User;
 
 @Repository("userRepository")
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 	
 	 User findByEmail(String email);
 	
