0cda320975019598fb6ee31b11c809b614bcb5eb
soumyadip007 <soumyadip.note@gmail.com>
2019-06-03 14:44:57 +0530

    Security configured

--- userauth/src/main/java/com/security/email/config/SecurityConfiguration.java
+++ userauth/src/main/java/com/security/email/config/SecurityConfiguration.java
@@ -13,8 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/register").permitAll()
-			.antMatchers("/confirm").permitAll()
-			.antMatchers("/resources/**").permitAll();
+			.antMatchers("/confirm").permitAll();
 		 
 	}
 
