75aa0ed518ddfafb8ce76d47523489109092c018
soumyadip007 <soumyadip.note@gmail.com>
2019-06-03 14:29:26 +0530

    JPA repository added

---
+++ userauth/src/main/java/com/security/email/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.security.email.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.security.email.entity.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	 User findByEmail(String email);
+	
+	 User findByConfirmationToken(String confirmationToken);
+}
