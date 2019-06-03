17522e8500205a4a02166c98503df305e258bd81
soumyadip007 <soumyadip.note@gmail.com>
2019-06-04 00:24:10 +0530

    Security web app init added

---
+++ userauth/src/main/java/com/security/email/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.security.email.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
