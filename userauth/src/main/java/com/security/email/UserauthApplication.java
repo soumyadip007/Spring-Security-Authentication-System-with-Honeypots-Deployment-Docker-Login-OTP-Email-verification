f0f7995db00fa7659e5a2d06335f92122bcf5db7
soumyadip007 <soumyadip.note@gmail.com>
2019-06-03 13:47:59 +0530

    Initial Commit

---
+++ userauth/src/main/java/com/security/email/UserauthApplication.java
@@ -0,0 +1,13 @@
+package com.security.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserauthApplication.class, args);
+	}
+
+}
