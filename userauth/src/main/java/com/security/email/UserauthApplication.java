46207bf2acf944cbe0ab2e6694a81fe31700f8eb
soumyadip007 <soumyadip.note@gmail.com>
2019-07-23 23:23:57 +0530

    import org.springframework.scheduling.annotation.*;

--- userauth/src/main/java/com/security/email/UserauthApplication.java
+++ userauth/src/main/java/com/security/email/UserauthApplication.java
@@ -3,6 +3,7 @@ package com.security.email;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.*;
 
 @SpringBootApplication
 @EnableAsync
